e15a82de6ab3cafae7fb35e72483ad0dd1392d7f
shinsangeun <s0813se@naver.com>
2021-10-31 23:49:12 +0900

    태그 클라우드 데이터 수정

--- lh_home/components/index/Tag.tsx
+++ lh_home/components/index/Tag.tsx
@@ -1,22 +1,67 @@
 // @ts-ignore
 import { TagCloud } from 'react-tagcloud';
-import data from './data/Word';
+import Word from './data/Word';
 // @ts-ignore
 import styled from "styled-components";
+import {useQuery} from "react-query";
+import {useState, useEffect} from "react";
 
 const Layout = styled.div`
     width: 1000px;height:300px;border-top: 1px solid #dcdcdc;padding: 10px;
 `;
 
 const Tag = () => {
+    const [tagData, setTagData] = useState([]);
+    const Word = ['서울', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '부산', '대구', '인천', '대전', '울산', '세종', '제주'];
+
+    useEffect(() => {
+       // Word.map((data, index) => {
+            handleData('경기');
+       // })
+    }, [])
+
+    async function handleData(location: string) {
+        console.log("location2:", location);
+
+        try{
+            await fetch(`http://apis.data.go.kr/B552555/lhLeaseNoticeInfo/lhLeaseNoticeInfo?serviceKey=vcu9zQh21aHdqeduiEp7Gr9QacLNM98A%2FWMExEIpgNQJwRyMSvNgP7ZJU3Ybpy75bM4nycmf%2FnP6IaLI2sXPUA%3D%3D&PG_SZ=10&PAGE=1&PAN_NM=${location}&UPP_AIS_TP_CD=06&PAN_SS=공고중`)
+            .then(res =>  console.log("res", res.json()))
+        }catch (e){
+            console.log("e:",e);
+
+        }
+
+        // const {isLoading, error, data} = useQuery('repoData', () =>
+        //     fetch(`http://apis.data.go.kr/B552555/lhLeaseNoticeInfo/lhLeaseNoticeInfo?serviceKey=vcu9zQh21aHdqeduiEp7Gr9QacLNM98A%2FWMExEIpgNQJwRyMSvNgP7ZJU3Ybpy75bM4nycmf%2FnP6IaLI2sXPUA%3D%3D&PG_SZ=10&PAGE=1&PAN_NM=${location}&UPP_AIS_TP_CD=06&PAN_SS=공고중`)
+        //         .then(res => res.json())
+        // )
+        //
+        // if (isLoading) return 'Loading...';
+        // if (error) return 'Error';
+        //
+        // if (data && data[1].dsList.length > 0){
+        //     let count = data[1].dsList.length;
+        //     console.log("data==>", location, data);
+        //        // setTagData(count);
+        //     // @ts-ignore
+        //     tagData.push({value: location, count: count});
+        // }else{
+        //     // @ts-ignore
+        //     tagData.push({value: location, count: 0});
+        // }
+    }
     return (
         <Layout>
-            <TagCloud
-                minSize={12}
-                maxSize={80}
-                tags={data}
-                onClick={(tag: { value: string; }) => alert(`'${tag.value}' was selected!`)}
-            />
+            {tagData.length > 0 ?
+                <TagCloud
+                    minSize={12}
+                    maxSize={80}
+                   // @ts-ignore
+                    tags={tagData}
+                    onClick={(tag: { value: string; }) => alert(`'${tag.value}' was selected!`)}
+                />
+                :<></>
+            }
         </Layout>
     )
 }
--- lh_home/components/index/data/Word.tsx
+++ lh_home/components/index/data/Word.tsx
@@ -1,66 +1,66 @@
 export default [
     {
         value: '서울특별시',
-        count: 38
+        count: 5
     },
     {
         value: '부산광역시',
-        count: 30
+        count: 2
     },
     {
         value: '대구광역시',
-        count: 28
+        count: 0
     },
     {
         value: '인천광역시',
-        count: 25
+        count: 0
     },
     {
         value: '대전광역시',
-        count: 33
+        count: 3
     },
     {
         value: '울산광역시',
-        count: 18
+        count: 4
     },
     {
         value: '세종특별자치시',
-        count: 20
+        count: 2
     },
     {
         value: '경기도',
-        count: 50
+        count: 5
     },
     {
         value: '강원도',
-        count: 30
+        count: 3
     },
     {
         value: '충청북도',
-        count: 35
+        count: 0
     },
     {
         value: '충청남도',
-        count: 54
+        count: 5
     },
     {
         value: '전라북도',
-        count: 33
+        count: 3
     },
     {
         value: '전라남도',
-        count: 66
+        count: 6
     },
     {
         value: '경상북도',
-        count: 26
+        count: 2
     },
     {
         value: '경상남도',
-        count: 40
+        count: 4
     },
     {
         value: '제주특별자치도',
-        count: 20
+        count: 0
     }
 ];
