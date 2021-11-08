556b29ea7287fe1df194e519a612ccfeb33b45f0
shinsangeun <s0813se@naver.com>
2021-11-09 00:44:06 +0900

    react-player 테스트 - playerList 생성

--- lh_home/pages/videoTest.tsx
+++ lh_home/pages/videoTest.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactPlayer from 'react-player/lazy';
 
-
 const videoTest = () => {
+    const [index, setIndex] = useState(0);
+    const [playList, setPlayList] = useState(null);
+
+    useEffect(() => {
+        // @ts-ignore
+        setPlayList({index:1, url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'})
+    })
+
+    if(playList === null){
+        return <p>Loading...</p>
+    }else{
+        return <Video playList={playList} index={index}/>
+    }
+
+}
+
+// @ts-ignore
+const Video = ({playList, index}) => {
+    const handleVideo = () => {
+        // index + 1;
+    }
+
     return (
         <>
             <h2>플레이어 테스트</h2>
             <div className='player-wrapper'>
                 <ReactPlayer
                     className='react-player'
-                    url='https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+                    url={playList.url}
                     width='800px'
                     height='500px'
                     playing={true}
@@ -18,6 +39,7 @@ const videoTest = () => {
                     light={false}
                     pip={true}
                     poster={'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'}
+                    onEnded={() => handleVideo()}
                 />
             </div>
         </>
