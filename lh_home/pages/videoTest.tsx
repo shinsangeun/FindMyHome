f3f2c92bc6a1dce810d5bfc1d1c6aedb5e04b488
shinsangeun <s0813se@naver.com>
2021-11-17 23:44:11 +0900

    react-player 테스트 - 여러개 영상 자동 재생 기능 추가

--- lh_home/pages/videoTest.tsx
+++ lh_home/pages/videoTest.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+/*import React, {useState, useEffect} from "react";
 import ReactPlayer from 'react-player/lazy';
 
 const videoTest = () => {
@@ -7,7 +7,11 @@ const videoTest = () => {
 
     useEffect(() => {
         // @ts-ignore
-        setPlayList({index:1, url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'})
+        setPlayList([
+            {index:1, url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
+            {index:2, url: 'http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8'},
+            {index:3, url: 'http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8'}
+        ])
     })
 
     if(playList === null){
@@ -26,11 +30,11 @@ const Video = ({playList, index}) => {
 
     return (
         <>
-            <h2>플레이어 테스트</h2>
+            <h2>Player Test</h2>
             <div className='player-wrapper'>
                 <ReactPlayer
                     className='react-player'
-                    url={playList.url}
+                    url={'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}
                     width='800px'
                     height='500px'
                     playing={true}
@@ -46,4 +50,58 @@ const Video = ({playList, index}) => {
     )
 }
 
-export default videoTest;
+export default videoTest;*/
+
+
+import ReactPlayer from 'react-player/lazy';
+import React, {useEffect, useState} from "react";
+
+type VideoPlayerProps = {
+    title: string;
+    vodPlaylistId: string;
+}
+
+const VideoPlayer = ({title, vodPlaylistId}: VideoPlayerProps) => {
+    const [playIndex, setPlayIndex] = useState(0);
+    const playList = [
+        {index:1, url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
+        {index:2, url: 'http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8'},
+        {index:3, url: 'http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8'}
+    ];
+
+    const handleNextVideo = (video: string | any[], playIndex: number) => {
+        if(playIndex === video.length - 1){
+            setPlayIndex(0);
+        }else{
+            setPlayIndex(playIndex + 1);
+        }
+    }
+
+    const selectVideo = (index: number) => {
+        setPlayIndex(index);
+    }
+
+    if(playList === null) return <p>Loading...</p>;
+
+    console.log("playList:", playList[playIndex].url)
+
+    return (
+        <>
+            <h2>Player Test</h2>
+            <ReactPlayer
+                // @ts-ignore
+                url={playList[playIndex].url}
+                playing
+                controls
+                muted
+                progressInterval={1000}
+                pip={true}
+                onEnded={() => {handleNextVideo(playList, playIndex)}}
+                width={'800px'}
+                height={'500px'}
+            />
+        </>
+    )
+}
+
+export default VideoPlayer;
