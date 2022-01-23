e5ec630d932fbc11697fa0a3f4675a30c6567586
shinsangeun <s0813se@naver.com>
2022-01-23 23:54:23 +0900

    카카오 api 지도 연동 - 샘플 테스트 추가

--- lh_home/components/data/MapContainer.tsx
+++ lh_home/components/data/MapContainer.tsx
@@ -1,24 +1,37 @@
-/*global kakao*/
-import React, { Component } from "react";
+/* global kakao */
+import React, {useEffect} from "react";
 // @ts-ignore
 import styled from "styled-components";
 
-class MapContainer extends Component {
-    componentDidMount() {
-        const script = document.createElement("script");
-        script.async = true;
-        script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=59ea7df9de6ace242c9fd3138e4d9ad8&autoload=false";
-        document.head.appendChild(script);
-    }
+const MapContents = styled.div`
+  width: 100%;height: 100%;padding: 20px;
+`;
 
-    render() {
-        return <MapContents id="Mymap"/>;
+declare global{
+    interface Window{
+        kakao: any;
     }
 }
 
-const MapContents = styled.div`
-  width: 100%;
-  height: 100%;
-`;
+const MapContainer = () => {
+    useEffect(() => {
+        const { kakao } = window;
+        let container = document.getElementById("map");
+
+        let options = {
+            center: new window.kakao.maps.LatLng(35.85133, 127.734086),
+            level: 13,
+        };
+
+        let map = new window.kakao.maps.Map(container, options);
+        console.log("loading kakaomap");
+    }, []);
+
+    return (
+        <MapContents className={"MapContents"}>
+            <div style={{width: '80%', height: '500px'}} className={"MapContents"} id="map"/>
+        </MapContents>
+    );
+};
 
 export default MapContainer;
